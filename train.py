992740c8d1ea9f3b20ca5c5e16a141c1cb410df3
owalid <othmane.el.ayadi@gmail.com>
2020-12-26 22:20:24 +0100

    in progress

--- train.py
+++ train.py
@@ -4,17 +4,23 @@ import matplotlib.pyplot as plt
 import math
 plt.rcParams['figure.figsize'] = (12.0, 9.0)
 
+def normalize_data(data):
+  min_data = data.min()
+  max_data = data.max()
+  result = (data - min_data) / (max_data - min_data)
+  return result
+
 def estimate_price(theta, X):
   return theta[0] + (theta[1] * X)
 
 def gradient_descent(X, y, theta, learning_rate, iterations):
-  m = len(y)
+  inv_m = 1 / len(y)
 
-  i = 0
-  while i < iterations:
-    theta[0] -= learning_rate * (1 / m) * sum(estimate_price(theta, X) - y)
-    theta[1] -= learning_rate * (1 / m) * sum(X * (estimate_price(theta, X) - y))
-    i += 1
+  for _ in range(iterations):
+    tmp_theta_0 = learning_rate * inv_m * sum(estimate_price(theta, X) - y)
+    tmp_theta_1 = learning_rate * inv_m * sum(X * (estimate_price(theta, X) - y))
+    theta[0] -= tmp_theta_0
+    theta[1] -= tmp_theta_1
   
   return theta
 
@@ -31,16 +37,16 @@ def read_csv(file):
 data = read_csv('data.csv')
 
 # convert array to vector
-x = data[0]
-y = data[1]
+x = normalize_data(data[0])
+y = normalize_data(data[1])
 
 # matix X
 # X = np.hstack((x, np.ones(x.shape)))
 
 theta = [0, 0]
-final_theta = gradient_descent(x, y, theta, 0.001, 1000)
+final_theta = gradient_descent(x, y, theta, 0.001, 10000)
 print(final_theta)
-
-plt.scatter(x, y)
-plt.plot(x, estimate_price(final_theta, x), c='r')
+print(estimate_price(final_theta, 60000))
+# plt.scatter(x, y)
+# plt.plot(x, estimate_price(final_theta, x), c='r')
 # plt.show()
